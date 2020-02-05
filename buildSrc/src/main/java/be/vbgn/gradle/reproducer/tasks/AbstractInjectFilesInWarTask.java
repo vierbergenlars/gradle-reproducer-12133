a99f33c4bc68e1a0d75c8d1f44213b5f3867c8ae
Lars Vierbergen <lars@vbgn.be>
2020-02-05 17:15:30 +0100

    Remove intermediate interfaces and classes

--- buildSrc/src/main/java/be/vbgn/gradle/reproducer/tasks/AbstractInjectFilesInWarTask.java
+++ buildSrc/src/main/java/be/vbgn/gradle/reproducer/tasks/AbstractInjectFilesInWarTask.java
@@ -2,15 +2,29 @@ package be.vbgn.gradle.reproducer.tasks;
 
 
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.internal.AbstractTask;
+import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.SkipWhenEmpty;
 
-abstract class AbstractInjectFilesInWarTask extends AbstractWarEnrichmentTask {
+abstract class AbstractInjectFilesInWarTask extends AbstractTask implements WarEnrichmentTask {
 
     /**
      * Files to inject in the war
      */
     private final ConfigurableFileCollection sourceFiles = getProject().files();
+
+    /**
+     * WAR file used as input (is not modified)
+     */
+    private RegularFileProperty inputWar = getProject().getObjects().fileProperty();
+
+    @InputFile
+    @Override
+    public RegularFileProperty getInputWar() {
+        return inputWar;
+    }
 
     @InputFiles
     @SkipWhenEmpty
--- buildSrc/src/main/java/be/vbgn/gradle/reproducer/tasks/WarEnrichmentTask.java
+++ buildSrc/src/main/java/be/vbgn/gradle/reproducer/tasks/WarEnrichmentTask.java
@@ -1,9 +1,23 @@
 package be.vbgn.gradle.reproducer.tasks;
 
+import org.gradle.api.Task;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.SkipWhenEmpty;
+
 /**
  * Created by thijs on 2/27/17.
  */
-public interface WarEnrichmentTask extends WarInputTask {
+public interface WarEnrichmentTask extends Task {
+
+    @InputFile
+    @SkipWhenEmpty
+    RegularFileProperty getInputWar();
+
+    default void setInputWar(FileCollection configuration) {
+        getInputWar().set(configuration::getSingleFile);
+    }
 
 
 }
