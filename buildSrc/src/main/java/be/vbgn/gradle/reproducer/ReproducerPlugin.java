195ed07af764a1abb40c8c2f8e7d0620e387bc20
Lars Vierbergen <lars@vbgn.be>
2020-02-05 17:25:21 +0100

    Remove interface that is not necessary to reproduce

--- buildSrc/src/main/java/be/vbgn/gradle/reproducer/ReproducerPlugin.java
+++ buildSrc/src/main/java/be/vbgn/gradle/reproducer/ReproducerPlugin.java
@@ -9,9 +9,10 @@ public class ReproducerPlugin implements Plugin<Project> {
     @Override
     public void apply(Project target) {
         target.getConfigurations().create("alfrescoAmp");
-        target.getTasks().create("applyAlfrescoAmp", InstallAmpsInWarTask.class, concreteTask -> {
+        InstallAmpsInWarTask task = target.getTasks().create("applyAlfrescoAmp", InstallAmpsInWarTask.class, concreteTask -> {
             concreteTask.setSourceFiles(target.getConfigurations().getByName("alfrescoAmp"));
             concreteTask.setInputWar(target.files("build.gradle"));
         });
+        System.out.println(task);
     }
 }
--- buildSrc/src/main/java/be/vbgn/gradle/reproducer/tasks/AbstractInjectFilesInWarTask.java
+++ buildSrc/src/main/java/be/vbgn/gradle/reproducer/tasks/AbstractInjectFilesInWarTask.java
@@ -2,13 +2,14 @@ package be.vbgn.gradle.reproducer.tasks;
 
 
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.internal.AbstractTask;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.SkipWhenEmpty;
 
-abstract class AbstractInjectFilesInWarTask extends AbstractTask implements WarEnrichmentTask {
+abstract class AbstractInjectFilesInWarTask extends AbstractTask {
 
     /**
      * Files to inject in the war
@@ -21,9 +22,12 @@ abstract class AbstractInjectFilesInWarTask extends AbstractTask implements WarE
     private RegularFileProperty inputWar = getProject().getObjects().fileProperty();
 
     @InputFile
-    @Override
     public RegularFileProperty getInputWar() {
         return inputWar;
+    }
+
+    public void setInputWar(FileCollection configuration) {
+        getInputWar().set(configuration::getSingleFile);
     }
 
     @InputFiles
